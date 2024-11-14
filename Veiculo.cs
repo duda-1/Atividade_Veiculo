6927c2e6d200d57315d6708e2d81a5d76929bf1e
Alunos <eduardacristinasilva2008@gmail.com>
2024-11-14 16:53:15 -0300

    Adicionar arquivos de projeto.

---
+++ Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Veiculo
+{
+    public  class Veiculo
+    {
+        public int id;
+        public string Tipo;
+        public string Modelo;
+        public int Ano;
+        public int CapacidadeTanque;
+        public int ConsumoPorKm;
+        public int DistanciaPercorrida;
+        public int ConsumoEstimado;
+    }
+}
